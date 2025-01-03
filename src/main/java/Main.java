0c5e18d875cedce20c6c18fb0dee77914ee9cb1c
gumburtu <aslanseval1986@gmail.com>
2025-01-03 19:56:18 +0000

    Version_04

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Degisiklik 2.");
         System.out.println("Degisiklik 3.");
+        System.out.println("Degisiklik 4.");
     }
 }
