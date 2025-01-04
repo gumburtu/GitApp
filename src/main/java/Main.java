5a6c9d731b7ef27de668feaee2f492055a7196f0
gumburtu <aslanseval1986@gmail.com>
2025-01-04 18:06:25 +0000

    v1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Degisiklik 2.");
-        System.out.println("Degisiklik 3.");
-        System.out.println("Degisiklik 4.");
     }
 }
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,14 +4,5 @@ public class Runner {
         //versiyon 1
         System.out.println("Degisiklik 1.");
 
-        //degisiklik yaptim versiyin 2
-        System.out.println("Degisiklik 2.");
-
-        //degisiklik yaptim versiyin 3
-        System.out.println("Degisiklik 3.");
-        System.out.println("Degisiklik 3.");
-        System.out.println("Degisiklik 3.");
-        System.out.println("Degisiklik 3.");
-        System.out.println("Degisiklik 3.");
     }
 }
